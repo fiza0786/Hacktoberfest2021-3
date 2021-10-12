96ee8994381869f07adcc65e07a77cd4811eb3dc
fiza0786 <92218448+fiza0786@users.noreply.github.com>
2021-10-12 23:44:07 +0530

    addition_begginner

---
+++ addition.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+void main()
+{
+ int x,y,result; // declear  
+ // addition  two number
+ // x = 3    y = 4    result =  x+y = 7
+
+printf("******Adition of two number****** \n");
+
+printf("ENter value of x and y \n"); //3
+scanf("%d  %d",&x,&y);                    // x define
+
+
+result =  x +y; // result  define
+// x = 3  y = 4  
+printf("%d + %d = %d\n",x,y,result);    
+    
+
+
+
+}
